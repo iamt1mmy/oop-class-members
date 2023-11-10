8bd5cfba7e144485017fa9f519e8e8f3eafdb103
TIMMY <timotei.adam@emanuel.ro>
2023-11-10 09:42:36 +0200

    examples with readonly fields

---
+++ OopClassMembers/OopClassMembers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace OopClassMembers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxLifespan = Person.MaxLifespanYears;
+            Person p = new Person("2344");
+            Console.WriteLine(p.FirstName); 
+            Console.WriteLine(p.LastName);
+            Console.WriteLine(p.CNP);
+
+            //Person.MaxLifespanYears
+            //p.CNP = "2344";
+            Person.PopulationCount = 1;
+        }
+    }
+}
